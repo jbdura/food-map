758480bb40eaf0d07a867c007725512e956c546e
jbdura <bidurajohnson@gmail.com>
2022-07-27 22:24:36 +0300

    Push 3

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Plenty of Land</h1>
+      <img src="hotpot.jpg" alt="food"/>
+    </div>
+  );
+}
+
+export default Home;
