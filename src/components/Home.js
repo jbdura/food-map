f7be8681bd39dff993ec438c74056212463604c3
jbdura <bidurajohnson@gmail.com>
2022-07-27 23:23:30 +0300

    README

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <h1>Plenty of Land</h1>
+      <h1>Food Map</h1>
       <img src="hotpot.jpg" alt="food"/>
     </div>
   );
